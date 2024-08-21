1e7b0bdc576b2322e0546406f0d3907181794d68
sidrxo <sidrxo@gmail.com>
2024-08-22 00:38:23 +0100

    removed localhost hardcode in login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,7 +8,7 @@ axios.defaults.withCredentials = true; // Ensure cookies are included in request
 
 const Login = () => {
     const handleGoogleLogin = () => {
-        window.location.href = 'http://localhost:5000/api/auth/google';
+        window.location.href = `${process.env.REACT_APP_API_BASE_URL}/api/auth/google`;
     };
 
     return (
