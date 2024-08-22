8d5c19e7b6b5dbf7af430dd8f82e2cec60e719f3
sidrxo <sidrxo@gmail.com>
2024-08-22 23:40:02 +0100

    hard coded in callback url

--- backend/login-server.js
+++ backend/login-server.js
@@ -60,7 +60,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL, // Use the environment variable here
+    callbackURL: 'https://server.anywh3re.xyz/api/auth/google/callback', // Use the environment variable here
 },
 async (accessToken, refreshToken, profile, done) => {
     try {
